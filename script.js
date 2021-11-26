0957a94617d3ba5214aa5fe44ddd92969f2d8b77
Mikhail Alpatov <alpatov70@mail.ru>
2021-11-26 17:09:59 +0300

    lesson2 dop

---
+++ script.js
@@ -0,0 +1,7 @@
+let num = 266219;
+num = num.toString();
+num.split(" ");
+let newNum = num[0]*num[1]*num[2]*num[3]*num[4]*num[5];
+console.log( newNum);
+console.log(String(newNum**3).slice(0,2));
+
