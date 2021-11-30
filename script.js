fbeea777f4b64cc4fb2659a97ec912aea3203877
Mikhail Alpatov <alpatov70@mail.ru>
2021-12-01 01:21:30 +0300

    lesson03

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-let num = 266219;
-num = num.toString();
-num.split(" ");
-let newNum = num[0]*num[1]*num[2]*num[3]*num[4]*num[5];
-console.log( newNum);
-console.log(String(newNum**3).slice(0,2));
-
+/* let lang = prompt("Выберите язык : (введите ru или en)");
+if (lang == "ru") {
+   console.log("Пн,Вт,Ср,Чт,Пт,Сб,Вс");
+}
+else if (lang == "en"){
+    console.log("Mo,Tu,We,Th,Fr,Sa,Su");
+}
+else{
+    alert("Вы допустили ошибку, попробуйте еще раз!");
+}
+let lang1 = prompt("Выберите язык : (введите ru или en)");
+switch(lang1){
+    case "ru" : {
+        console.log("Пн,Вт,Ср,Чт,Пт,Сб,Вс");
+        break;
+    }
+    case "en"  : {
+        console.log("Mo,Tu,We,Th,Fr,Sa,Su");
+        break;
+    }
+    default : alert("Вы допустили ошибку, попробуйте еще раз!");
+    break;
+    } */
+let langArray = [ ] ;
+langArray['ru'] = ['Пн','Вт','Ср','Чт','Пт','Сб','Вс'];
+langArray['en'] = ['Mo','Tu','We','Th','Fr','Sa','Su'];
+let lang2 =  prompt("Выберите язык : (введите ru или en)");
+console.log(langArray[lang2]);
