d2475d46ec7d88a443efb44e0bf7528e418c7a61
Mikhail Alpatov <alpatov70@mail.ru>
2021-12-02 14:19:26 +0300

    Lesson04

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+let value = prompt("Введите необходимое значение");
+const argument = function (value1) {
+    if (typeof value1 !== "string") {
+       return 'Необходимо ввести строчное значение';
+    } else {
+       value1 = value1.trim();
+       if (value1.length>30) {
+           return value1.slice(0,29) + '...' ;
+       }
+       else {
+       return value1;
+       }
+    }
+ };
+ alert(argument(value));
