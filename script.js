17e33804b3baa50c71becf6f54d637f4bd752017
Mikhail Alpatov <alpatov70@mail.ru>
2021-12-06 11:51:35 +0300

    lesson07

--- script.js
+++ script.js
@@ -1,35 +1,22 @@
 'use strict';
- let arr= ['123423','45545','7899','2201','362','21','41228'];
+const week = ['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье'];
+const day = document.querySelector('.days');
+const today = new Date();
 
-const value = function(){
-   for (let i=0; i<7; i++){
-      if (arr[i].startsWith('2') || arr[i].startsWith('4')){
-        console.log( arr[i]);
+const days = () => {
+   week.forEach((item, i) => {
+      const div = document.createElement('div'); 
+      if (i === +today.getDay() -1) {
+        div.textContent = week[i]; 
+         div.classList.add('today');        
       }
- 
-    
-   }
- 
+      if (item == 'Суббота' || item == 'Воскресенье') { 
+         div.textContent = week[i]; 
+         div.classList.add('italic');
+      } else {
+         div.textContent = week[i]; 
+      }
+      day.appendChild(div); 
+   });
 };
-value();
-
- const simpleValue = function(){
-
-for (let i = 2; i <= 100; i++) {
-    let check = 1;
-    if ( i % 2 != 0)
-    {
-        for (let j = 3; j*j <= i ; j+=2)
-        {
-            if (i%j==0)
-            {
-                check=0;
-                break;
-            }
-        }
-    }
-    else if (i != 2) {check = 0;}
-    if (check==1) {console.log(i);}
-}
- };
-simpleValue();
+days(); 
